b91e7d4d5f6b7948ef6efa11d9a6aca89aa1c23e
barryos112 <barryos112@gmail.com>
2023-01-29 16:35:51 +0000

    Added account creation page, linked to home page

--- final-year-project/src/Components/Account_creation/account_creation.js
+++ final-year-project/src/Components/Account_creation/account_creation.js
@@ -1,86 +1,144 @@
-// import React from "react";
+// // import React from "react";
 
-// function Home() {
-//   return <h1>Account creation page</h1>;
-// }
+// // function Home() {
+// //   return <h1>Account creation page</h1>;
+// // }
+
+// // export default Home;
+
+// import React, { useState } from "react";
+
+// const useStyles = {
+//   container: {
+//     display: "flex",
+//     flexDirection: "column",
+//     alignItems: "center",
+//     padding: "20px",
+//   },
+//   formField: {
+//     margin: "10px",
+//     padding: "10px",
+//     borderRadius: "5px",
+//     border: "1px solid grey",
+//   },
+//   submitButton: {
+//     margin: "20px",
+//     padding: "10px",
+//     backgroundColor: "blue",
+//     color: "white",
+//     borderRadius: "5px",
+//     border: "none",
+//     "&:hover": {
+//       backgroundColor: "darkblue",
+//     },
+//   },
+// };
+
+// const Home = () => {
+//   const [formData, setFormData] = useState({
+//     email: "",
+//     password: "",
+//     name: "",
+//   });
+
+//   const handleInputChange = (event) => {
+//     const { name, value } = event.target;
+//     setFormData({ ...formData, [name]: value });
+//   };
+
+//   const handleSubmit = (event) => {
+//     event.preventDefault();
+//     console.log(formData);
+//   };
+
+//   return (
+//     <form onSubmit={handleSubmit} style={useStyles.container}>
+//       <input
+//         name="email"
+//         placeholder="Email"
+//         type="email"
+//         value={formData.email}
+//         onChange={handleInputChange}
+//         style={useStyles.formField}
+//       />
+//       <input
+//         name="password"
+//         placeholder="Password"
+//         type="password"
+//         value={formData.password}
+//         onChange={handleInputChange}
+//         style={useStyles.formField}
+//       />
+//       <input
+//         name="name"
+//         placeholder="Name"
+//         type="text"
+//         value={formData.name}
+//         onChange={handleInputChange}
+//         style={useStyles.formField}
+//       />
+//       <button type="submit" style={useStyles.submitButton}>
+//         Create Account
+//       </button>
+//     </form>
+//   );
+// };
 
 // export default Home;
 
 import React, { useState } from "react";
-
-const useStyles = {
-  container: {
-    display: "flex",
-    flexDirection: "column",
-    alignItems: "center",
-    padding: "20px",
-  },
-  formField: {
-    margin: "10px",
-    padding: "10px",
-    borderRadius: "5px",
-    border: "1px solid grey",
-  },
-  submitButton: {
-    margin: "20px",
-    padding: "10px",
-    backgroundColor: "blue",
-    color: "white",
-    borderRadius: "5px",
-    border: "none",
-    "&:hover": {
-      backgroundColor: "darkblue",
-    },
-  },
-};
+import "./AccountCreation.css";
 
 const Home = () => {
-  const [formData, setFormData] = useState({
-    email: "",
-    password: "",
-    name: "",
-  });
+  const [username, setUsername] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
 
-  const handleInputChange = (event) => {
-    const { name, value } = event.target;
-    setFormData({ ...formData, [name]: value });
-  };
-
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    console.log(formData);
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // Validate form inputs and submit to server
   };
 
   return (
-    <form onSubmit={handleSubmit} style={useStyles.container}>
-      <input
-        name="email"
-        placeholder="Email"
-        type="email"
-        value={formData.email}
-        onChange={handleInputChange}
-        style={useStyles.formField}
-      />
-      <input
-        name="password"
-        placeholder="Password"
-        type="password"
-        value={formData.password}
-        onChange={handleInputChange}
-        style={useStyles.formField}
-      />
-      <input
-        name="name"
-        placeholder="Name"
-        type="text"
-        value={formData.name}
-        onChange={handleInputChange}
-        style={useStyles.formField}
-      />
-      <button type="submit" style={useStyles.submitButton}>
-        Create Account
-      </button>
-    </form>
+    <div className="form-container">
+      <h1 className="form-header">Create an Account</h1>
+      <form onSubmit={handleSubmit}>
+        <input
+          className="form-input"
+          type="text"
+          placeholder="Username"
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+          required
+        />
+        <input
+          className="form-input"
+          type="email"
+          placeholder="Email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          required
+        />
+        <input
+          className="form-input"
+          type="password"
+          placeholder="Password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          required
+        />
+        <input
+          className="form-input"
+          type="password"
+          placeholder="Confirm Password"
+          value={confirmPassword}
+          onChange={(e) => setConfirmPassword(e.target.value)}
+          required
+        />
+        <input className="form-submit" type="submit" value="Sign Up" />
+      </form>
+    </div>
   );
 };
 
--- final-year-project/src/Components/Landing_page/landing_page.js
+++ final-year-project/src/Components/Landing_page/landing_page.js
@@ -1,11 +1,22 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 import "./landing_page.css";
 import Card from "react-bootstrap/Card";
 
 const Home = () => {
   return (
-    <div className="card">
+    <div className="card-container">
+      <Card style={{ width: "18rem" }}>
+        {/* <Card.Img variant="top" src="holder.js/100px180" /> */}
+        <Card.Body>
+          <Card.Title>Card Title</Card.Title>
+          <Card.Text>
+            Some quick example text to build on the card title and make up the
+            bulk of the card's content.
+          </Card.Text>
+        </Card.Body>
+      </Card>
       <Card style={{ width: "18rem" }}>
         {/* <Card.Img variant="top" src="holder.js/100px180" /> */}
         <Card.Body>
@@ -22,7 +33,9 @@ const Home = () => {
           <h1>Welcome to the future of Fantasy Sports</h1>
         </div>
         <div className="home-page-body">
-          <button className="join-button">Join a League</button>
+          <Link to="/create-account">
+            <button className="join-button">Create an account</button>
+          </Link>
         </div>
       </div>
     </div>
