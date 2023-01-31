88d2e5d7a75fab9ff59e7bc812925e3bd9e9c9be
barryos112 <barryos112@gmail.com>
2023-01-31 16:57:47 +0000

    Cards to landing page and fixed BG

--- final-year-project/src/Components/Landing_page/landing_page.js
+++ final-year-project/src/Components/Landing_page/landing_page.js
@@ -6,36 +6,34 @@ import Card from "react-bootstrap/Card";
 
 const Home = () => {
   return (
-    <div className="card-container">
-      <Card style={{ width: "18rem" }}>
-        {/* <Card.Img variant="top" src="holder.js/100px180" /> */}
-        <Card.Body>
-          <Card.Title>Card Title</Card.Title>
-          <Card.Text>
-            Some quick example text to build on the card title and make up the
-            bulk of the card's content.
-          </Card.Text>
-        </Card.Body>
-      </Card>
-      <Card style={{ width: "18rem" }}>
-        {/* <Card.Img variant="top" src="holder.js/100px180" /> */}
-        <Card.Body>
-          <Card.Title>Card Title</Card.Title>
-          <Card.Text>
-            Some quick example text to build on the card title and make up the
-            bulk of the card's content.
-          </Card.Text>
-        </Card.Body>
-      </Card>
+    <div className="home-page-container">
+      <div className="home-page-header">
+        <h1>Welcome to the future of Fantasy Sports</h1>
+      </div>
+      <div className="home-page-body">
+        <Link to="/create-account">
+          <button className="join-button">Create an account</button>
+        </Link>
+      </div>
 
-      <div className="home-page-container">
-        <div className="home-page-header">
-          <h1>Welcome to the future of Fantasy Sports</h1>
+      <div className="card-container">
+        <div className="card-outer">
+          <Card className="card-inner">
+            <Card.Title>Welcome</Card.Title>
+            <Card.Text>Explain Main Rules</Card.Text>
+          </Card>
         </div>
-        <div className="home-page-body">
-          <Link to="/create-account">
-            <button className="join-button">Create an account</button>
-          </Link>
+        <div className="card-outer">
+          <Card className="card-inner">
+            <Card.Title>Next steps</Card.Title>
+            <Card.Text>Create account pick team</Card.Text>
+          </Card>
+        </div>
+        <div className="card-outer">
+          <Card className="card-inner">
+            <Card.Title>Enjoy</Card.Title>
+            <Card.Text>Play and challenge friends</Card.Text>
+          </Card>
         </div>
       </div>
     </div>
