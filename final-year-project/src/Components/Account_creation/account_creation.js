f8b9e82c3465fec4a13218033a6d848ab580cee8
barryos112 <barryos112@gmail.com>
2023-02-22 20:08:58 +0000

    Firebase set up for storing users

--- final-year-project/src/Components/Account_creation/account_creation.js
+++ final-year-project/src/Components/Account_creation/account_creation.js
@@ -1,60 +1,4 @@
-// import React, { useState } from "react";
-// import "./AccountCreation.css";
-
-// const Home = () => {
-//   const [username, setUsername] = useState("");
-//   const [email, setEmail] = useState("");
-//   const [password, setPassword] = useState("");
-//   const [confirmPassword, setConfirmPassword] = useState("");
-
-//   const handleSubmit = (e) => {
-//     e.preventDefault();
-//     // Validate form inputs and submit to server
-//   };
-
-//   return (
-//     <div className="form-container">
-//       <h1 className="form-header">Create an Account</h1>
-//       <form onSubmit={handleSubmit}>
-//         <input
-//           className="form-input"
-//           type="text"
-//           placeholder="Username"
-//           value={username}
-//           onChange={(e) => setUsername(e.target.value)}
-//           required
-//         />
-//         <input
-//           className="form-input"
-//           type="email"
-//           placeholder="Email"
-//           value={email}
-//           onChange={(e) => setEmail(e.target.value)}
-//           required
-//         />
-//         <input
-//           className="form-input"
-//           type="password"
-//           placeholder="Password"
-//           value={password}
-//           onChange={(e) => setPassword(e.target.value)}
-//           required
-//         />
-//         <input
-//           className="form-input"
-//           type="password"
-//           placeholder="Confirm Password"
-//           value={confirmPassword}
-//           onChange={(e) => setConfirmPassword(e.target.value)}
-//           required
-//         />
-//         <input className="form-submit" type="submit" value="Sign Up" />
-//       </form>
-//     </div>
-//   );
-// };
-
-// export default Home;
+import { useNavigate } from "react-router-dom";
 
 import React, { useState } from "react";
 import "./AccountCreation.css";
@@ -70,6 +14,8 @@ import "firebase/compat/auth";
 
 import "firebase/compat/firestore";
 
+import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
+
 // Initialize Firebase
 const firebaseConfig = {
   apiKey: "AIzaSyBNRynRskEMrHDDxJuDME1Xa6MNLTDKXsg",
@@ -83,11 +29,15 @@ const firebaseConfig = {
 };
 firebase.initializeApp(firebaseConfig);
 
+const auth = getAuth();
+const db = getDatabase();
+
 const Home = () => {
   const [username, setUsername] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [confirmPassword, setConfirmPassword] = useState("");
+  const navigate = useNavigate();
 
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -97,15 +47,28 @@ const Home = () => {
       return;
     }
 
-    // Submit to server
-    const userData = {
-      username: username,
-      email: email,
-      password: password,
-    };
+    console.log("pressed submit");
+    createUserWithEmailAndPassword(auth, email, password)
+      .then((userCredential) => {
+        // Signed in
+        const user = userCredential.user;
+        console.log("created user", user.uid);
+        // Submit to server
+        const userData = {
+          username: username,
+          email: email,
+          password: password,
+        };
 
-    const db = getDatabase();
-    set(ref(db, "users/" + Date.now()), userData);
+        set(ref(db, "users/" + user.uid), userData);
+        navigate("/select-sport"); // navigate to /path/to/page
+      })
+      .catch((error) => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        console.log(" error", errorMessage);
+        // ..
+      });
 
     // Clear form inputs
     setUsername("");
